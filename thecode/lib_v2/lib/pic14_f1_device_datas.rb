257c36ef69238fef392a508d27b6ad64a204e608
Mr. T <mrtee@gmx.com>
2012-08-31 13:32:29 +0200

    added PIC12F1840 and PIC16F1847

--- thecode/lib_v2/lib/pic14_f1_device_datas.rb
+++ thecode/lib_v2/lib/pic14_f1_device_datas.rb
@@ -1,5 +1,33 @@
 module PICkit2
+
+  # hint
+  # programMemory: datasheet*2
+  # dataMemory: 1e000+datasheet*2
+  # configurationMemory: 10000+datasheet*2
+
   PIC14F1DeviceData = 
+    {
+    :deviceName => "PIC12F1840",
+    :deviceID => 0b011011100,
+    :programMemory => (0..0x1fff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 32,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
+    {
+    :deviceName => "PIC16F1847",
+    :deviceID => 0b010100100,
+    :programMemory => (0..0x3fff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 32,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
     {
     :deviceName => "PIC16F1825",
     :deviceID => 0b100111011,
