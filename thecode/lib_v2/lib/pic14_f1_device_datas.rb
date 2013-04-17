837c1d2028941695453bd6b5e50da3089707de7e
Mr. T <mrtee@gmx.com>
2013-04-17 22:57:14 +0200

    PIC16F1938, PIC16LF1938 added

--- thecode/lib_v2/lib/pic14_f1_device_datas.rb
+++ thecode/lib_v2/lib/pic14_f1_device_datas.rb
@@ -51,6 +51,28 @@ module PICkit2
     :configurationWord2 => 0x10010
   },
     {
+    :deviceName => "PIC12F1938",
+    :deviceID => 0b100011101,
+    :programMemory => (0..0x7fff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 8,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
+    {
+    :deviceName => "PIC12LF1938",
+    :deviceID => 0b100100101,
+    :programMemory => (0..0x7fff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 8,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
+    {
     :deviceName => "PIC12LF1822",
     :deviceID => 0b101000000,
     :programMemory => (0..0xfff),
