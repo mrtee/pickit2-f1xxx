7bb6a26c864f932ea7ffb134344b8d518941761a
Mr. T <mrtee@gmx.com>
2013-10-01 13:34:22 +0200

    Adding to support PIC16F1827 and PIC16LF1827

--- thecode/lib_v2/lib/pic14_f1_device_datas.rb
+++ thecode/lib_v2/lib/pic14_f1_device_datas.rb
@@ -135,6 +135,28 @@ module PICkit2
     :configurationWord2 => 0x10010
   },
     {
+    :deviceName => "PIC12F1827",
+    :deviceID => 0b100111101,
+    :programMemory => (0..0xfff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 8,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
+    {
+    :deviceName => "PIC12LF1827",
+    :deviceID => 0b101000101,
+    :programMemory => (0..0xfff),
+    :dataMemory => (0x1e000..0x1e1ff),
+    :configurationMemory => (0x10000..0x103ff),
+    :rowSize => 32,
+    :numberOfLatches => 8,
+    :configurationWord1 => 0x1000e,
+    :configurationWord2 => 0x10010
+  },
+    {
     :deviceName => "PIC12F1822",
     :deviceID => 0b100111000,
     :programMemory => (0..0xfff),
